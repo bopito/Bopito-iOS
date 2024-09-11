fc2a73bccd01128f05389eca4f66b32e663d6ee3
HansHeidmann <hansheidmann@gmail.com>
2024-09-10 18:36:20 -0700

    reply system working, random avatars, and more

---
+++ Bopito/Tools/DateTimeTool.swift
@@ -0,0 +1,8 @@
+//
+//  DateTimeTool.swift
+//  Bopito
+//
+//  Created by Hans Heidmann on 9/10/24.
+//
+
+import Foundation
---
+++ Bopito/Tools/RandomGeneratorTool.swift
@@ -0,0 +1,8 @@
+//
+//  RandomGeneratorTool.swift
+//  Bopito
+//
+//  Created by Hans Heidmann on 9/10/24.
+//
+
+import Foundation
